9b62177d39e868a7f8b19a4aee0847a9d7705270
Luis Bolanos <luis.bolanos01@telusinternational.com>
2022-08-18 17:11:49 -0600

    adding initial structure project

---
+++ ProjectGames/src/main/java/com/ProjectGames/ProjectGamesApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGamesApplication.class, args);
+	}
+
+}
---
+++ ProjectGames/src/test/java/com/ProjectGames/ProjectGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
